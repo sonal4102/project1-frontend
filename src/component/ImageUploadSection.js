1ef06004a9eb42f055acd22fa952db7f5a92d82d
sonal <sonalsingh4102@gmail.com>
2023-05-14 19:06:24 +0530

    image

--- src/component/ImageUploadSection.js
+++ src/component/ImageUploadSection.js
@@ -1,189 +1,131 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   AspectRatio,
   Box,
+  Button,
   Container,
   Heading,
+  Image,
   Input,
+  SimpleGrid,
   Stack,
   Text,
 } from "@chakra-ui/react";
 import { motion, useAnimation } from "framer-motion";
 
-const first = {
-  rest: {
-    rotate: "-15deg",
-    scale: 0.95,
-    x: "-50%",
-    filter: "grayscale(80%)",
-    transition: {
-      duration: 0.5,
-      type: "tween",
-      ease: "easeIn",
-    },
-  },
-  hover: {
-    x: "-70%",
-    scale: 1.1,
-    rotate: "-20deg",
-    filter: "grayscale(0%)",
-    transition: {
-      duration: 0.4,
-      type: "tween",
-      ease: "easeOut",
-    },
-  },
-};
+export default function ImageUploadSection() {
+  const controls = useAnimation();
+  const startAnimation = () => controls.start("hover");
+  const stopAnimation = () => controls.stop();
+  const [image, setImage] = useState("");
+  const [allImage, setAllImage] = useState([]);
 
-const second = {
-  rest: {
-    rotate: "15deg",
-    scale: 0.95,
-    x: "50%",
-    filter: "grayscale(80%)",
-    transition: {
-      duration: 0.5,
-      type: "tween",
-      ease: "easeIn",
-    },
-  },
-  hover: {
-    x: "70%",
-    scale: 1.1,
-    rotate: "20deg",
-    filter: "grayscale(0%)",
-    transition: {
-      duration: 0.4,
-      type: "tween",
-      ease: "easeOut",
-    },
-  },
-};
 
-const third = {
-  rest: {
-    scale: 1.1,
-    filter: "grayscale(80%)",
-    transition: {
-      duration: 0.5,
-      type: "tween",
-      ease: "easeIn",
-    },
-  },
-  hover: {
-    scale: 1.3,
-    filter: "grayscale(0%)",
-    transition: {
-      duration: 0.4,
-      type: "tween",
-      ease: "easeOut",
-    },
-  },
-};
+  function covertToBase64(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const base64Image = reader.result;
+        setImage(base64Image);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
-const PreviewImage = React.forwardRef((props, ref) => {
+  useEffect(() => {
+    getImage();
+  }, []);
+
+  function uploadImage() {
+    if (image) {
+      fetch("http://localhost:5000/upload-image", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          base64: image,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          getImage(); // Fetch the updated collection of images
+          setImage(""); // Reset the selected image
+        });
+    }
+  }
+
+  function getImage() {
+    fetch("http://localhost:5000/get-image", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAllImage(data.data);
+      });
+  }
+
   return (
-    <Box
-      bg="white"
-      top="0"
-      height="100%"
-      width="100%"
-      position="absolute"
-      borderWidth="1px"
-      borderStyle="solid"
-      rounded="sm"
-      borderColor="gray.400"
-      as={motion.div}
-      backgroundSize="cover"
-      backgroundRepeat="no-repeat"
-      backgroundPosition="center"
-      backgroundImage={`url("https://image.shutterstock.com/image-photo/paella-traditional-classic-spanish-seafood-600w-1662253543.jpg")`}
-      {...props}
-      ref={ref}
-    />
-  );
-});
-  
-  export default function ImageUploadSection() {
-    const controls = useAnimation();
-    const startAnimation = () => controls.start("hover");
-    const stopAnimation = () => controls.stop();
-    return (
-      <Container my="12" mx="16%">
-        <AspectRatio width="64" ratio={1}>
-          <Box
-            borderColor="gray.300"
-            borderStyle="dashed"
-            borderWidth="2px"
-            rounded="md"
-            shadow="sm"
-            role="group"
-            transition="all 150ms ease-in-out"
-            _hover={{
-              shadow: "md"
-            }}
-            as={motion.div}
-            initial="rest"
-            animate="rest"
-            whileHover="hover"
-          >
-            <Box position="relative" height="100%" width="100%">
-              <Box
-                position="absolute"
-                top="0"
-                left="0"
+    <>
+      <Container my="12">
+        <Box
+          position="relative"
+          border="1px solid grey"
+          borderRadius="5px"
+          width="50%"
+          mx="auto"
+        >
+          <Stack p="2" textAlign="center">
+            <Text fontWeight="bold">Select File/Image</Text>
+          </Stack>
+          <Input
+            type="file"
+            position="absolute"
+            top="0"
+            left="0"
+            opacity="0"
+            aria-hidden="true"
+            accept="image/*"
+            onDragEnter={startAnimation}
+            onDragLeave={stopAnimation}
+            onChange={covertToBase64}
+          />
+        </Box>
+      </Container>
+      <Container>
+        {image ? (
+          <Image src={image} height={40} alt="image" />
+        ) : null}
+        <Button
+          colorScheme="blue"
+          mx="auto"
+          display="block"
+          mt={4}
+          onClick={uploadImage}
+          disabled={!image}
+        >
+          Upload your image
+        </Button>
+      </Container>
+      <Container overflow="auto" maxH={280} p={0} position="relative">
+        <SimpleGrid columns={4} spacing={4} p={4}>
+          {allImage.map((data, index) => (
+            <Box key={index} height="80px" position="relative">
+              <Image
                 height="100%"
                 width="100%"
-                display="flex"
-                flexDirection="column"
-              >
-                <Stack
-                  height="100%"
-                  width="100%"
-                  display="flex"
-                  alignItems="center"
-                  justify="center"
-                  spacing="4"
-                >
-                  <Box height="16" width="12" position="relative">
-                    <PreviewImage
-                      variants={first}
-                      backgroundImage="url('https://image.shutterstock.com/image-photo/paella-traditional-classic-spanish-seafood-600w-1662253543.jpg')"
-                    />
-                    <PreviewImage
-                      variants={second}
-                      backgroundImage="url('https://images.unsplash.com/photo-1565299585323-38d6b0865b47?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2628&q=80')"
-                    />
-                    <PreviewImage
-                      variants={third}
-                      backgroundImage={`url("https://images.unsplash.com/photo-1563612116625-3012372fccce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2480&q=80")`}
-                    />
-                  </Box>
-                  <Stack p="8" textAlign="center" spacing="1">
-                    <Heading fontSize="lg" color="gray.700" fontWeight="bold">
-                      Drop images here
-                    </Heading>
-                    <Text fontWeight="light">or click to upload</Text>
-                  </Stack>
-                </Stack>
-              </Box>
-              <Input
-                type="file"
-                height="100%"
-                width="100%"
-                position="absolute"
-                top="0"
-                left="0"
-                opacity="0"
-                aria-hidden="true"
-                accept="image/*"
-                onDragEnter={startAnimation}
-                onDragLeave={stopAnimation}
+                src={data.image}
+                objectFit="cover"
+                borderRadius="md"
               />
             </Box>
-          </Box>
-        </AspectRatio>
+          ))}
+        </SimpleGrid>
       </Container>
-    );
-  }
-  
+    </>
+  );
+}
